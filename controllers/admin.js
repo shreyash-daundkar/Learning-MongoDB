fe95c080fdafc9a5d77d68f88448caf909727c53
Shreyash Daundkar <productiveappa@gmail.com>
2024-01-16 21:42:37 +0530

    Modified all controllers related to edit product.

--- controllers/admin.js
+++ controllers/admin.js
@@ -33,14 +33,11 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  req.user
-    .getProducts({ where: { id: prodId } })
-    // Product.findById(prodId)
-    .then(products => {
-      const product = products[0];
+  Product.findById(prodId)
+    .then(product => {
       if (!product) {
         return res.redirect('/');
-      }
+      }  
       res.render('admin/edit-product', {
         pageTitle: 'Edit Product',
         path: '/admin/edit-product',
@@ -52,18 +49,11 @@ exports.getEditProduct = (req, res, next) => {
 };
 
 exports.postEditProduct = (req, res, next) => {
-  const prodId = req.body.productId;
-  const updatedTitle = req.body.title;
-  const updatedPrice = req.body.price;
-  const updatedImageUrl = req.body.imageUrl;
-  const updatedDesc = req.body.description;
-  Product.findById(prodId)
+  const { title, imageUrl, price, description, productId } = req.body;
+  Product.findById(productId)
     .then(product => {
-      product.title = updatedTitle;
-      product.price = updatedPrice;
-      product.description = updatedDesc;
-      product.imageUrl = updatedImageUrl;
-      return product.save();
+      const newProduct = new Product(title, imageUrl, price, description, productId);
+      return newProduct.save();
     })
     .then(result => {
       console.log('UPDATED PRODUCT!');
@@ -73,8 +63,7 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  req.user
-    .getProducts()
+  Product.findAll()
     .then(products => {
       res.render('admin/products', {
         prods: products,
