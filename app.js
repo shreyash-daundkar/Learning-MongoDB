93f8e0fe63cbeb7b724fbb6ceab49f77c030f596
Shreyash Daundkar <productiveappa@gmail.com>
2024-01-16 08:34:17 +0530

    Open add product related routes

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-// const adminRoutes = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 // const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -32,7 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 //     .catch(err => console.log(err));
 // });
 
- // app.use('/admin', adminRoutes);
+ app.use('/admin', adminRoutes);
 // app.use(shopRoutes);
 
 // app.use(errorController.get404);
@@ -71,4 +71,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 //     console.log(err);
 //   });
 
-mongoConnect(() => app.listen(4000));
+mongoConnect(() => {
+    console.log('db')
+    app.listen(4000);
+});
