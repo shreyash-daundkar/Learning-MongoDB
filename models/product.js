f5537a592826802d82521792e0941d73f39822b2
Shreyash Daundkar <productiveappa@gmail.com>
2024-01-17 10:24:55 +0530

    Added delete product by id functinality in product class

--- models/product.js
+++ models/product.js
@@ -1,61 +1,62 @@
 const mongodb = require('mongodb');
 const { getDb } = require('../util/database');
 
+
+
 class Product {
 
-  constructor(title, imageUrl, price, description, id) {
+
+  constructor(title, imageUrl, price, description, id, userId) {
+    
     this.title = title;
     this.image = imageUrl;
     this.price = price;
     this.description = description;
     this.id = id;
+    this.userId = userId;
   }
+
 
   async save() {
     const db = getDb();
+
     if(this.id) {
+
       return await db.collection('product').updateOne({
         _id: new mongodb.ObjectId(this.id),
       },  { $set: this });
     }
+
     return await db.collection('product').insertOne(this);
   }
 
+
   static async findAll() {
     const db = getDb();
+
     return await db.collection('product').find().toArray();
   }
+
 
   static async findById(prodId) {
     const db = getDb();
 
-    return await db.collection('product').find({
+    return await db.collection('product').findOne({
       _id: new mongodb.ObjectId(prodId)
-    }).next();
+    });
+  }
+
+
+  static async deleteById(prodId) {
+    const db = getDb();
+
+    return await db.collection('product').deleteOne({
+      _id: new mongodb.ObjectId(prodId)
+    });
   }
 
 }
 
-// const Product = sequelize.define('product', {
-//   id: {
-//     type: Sequelize.INTEGER,
-//     autoIncrement: true,
-//     allowNull: false,
-//     primaryKey: true
-//   },
-//   title: Sequelize.STRING,
-//   price: {
-//     type: Sequelize.DOUBLE,
-//     allowNull: false
-//   },
-//   imageUrl: {
-//     type: Sequelize.STRING,
-//     allowNull: false
-//   },
-//   description: {
-//     type: Sequelize.STRING,
-//     allowNull: false
-//   }
-// });
+
 
 module.exports = Product;
