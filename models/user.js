e140924ae8599959eb06dcc2183f2f606d5e7f84
Shreyash Daundkar <productiveappa@gmail.com>
2024-01-18 13:26:14 +0530

    Added save cart functinality in product class

--- models/user.js
+++ models/user.js
@@ -6,11 +6,12 @@ const { getDb } = require('../util/database');
 class User {
 
 
-  constructor(id, name, email) {
+  constructor(id, name, email, cart) {
 
     this._id = new mongodb.ObjectId(id);
     this.name = name;
     this.email = email;
+    this.cart = cart;
   }
 
 
@@ -27,6 +28,13 @@ class User {
     return await db.collection('users').findOne({
        _id: new mongodb.ObjectId(id) 
     });
+  }
+
+
+  static async saveCart(userId, updatedCart) {
+    const db = getDb();
+
+    return await db.collection('users').updateOne({ _id: userId },  { $set: { cart: updatedCart} });
   }
   
     
