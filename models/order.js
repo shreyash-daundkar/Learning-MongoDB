062bb3294b38b4ce8f21d415f75380a5d019ae12
Shreyash Daundkar <productiveappa@gmail.com>
2024-01-19 11:21:03 +0530

    Created order class to store orders in mongodb database

--- models/order.js
+++ models/order.js
@@ -1,14 +1,27 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const { getDb } = require('../util/database');
 
-const sequelize = require('../util/database');
 
-const Order = sequelize.define('order', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
+
+class Order {
+
+
+  constructor(userId, cart) {
+
+    this.userId = new mongodb.ObjectId(userId);
+    this.items = cart;
+    this.date = new Date();
   }
-});
+
+
+  async save() {
+    const db = getDb();
+      
+    return await db.collection('orders').insertOne(this);
+  }
+
+}
+
+
 
 module.exports = Order;
