e37ec3838a1f0b9e4ae87aa2bef2b230c1965079
Shreyash Daundkar <productiveappa@gmail.com>
2024-01-21 12:50:09 +0530

    Added product model using mongoose

--- models/product.js
+++ models/product.js
@@ -1,62 +1,24 @@
-const mongodb = require('mongodb');
-const { getDb } = require('../util/database');
+const { Schema, model } = require('mongoose');
 
 
-
-class Product {
-
-
-  constructor(title, imageUrl, price, description, id, userId) {
-    
-    this.title = title;
-    this.image = imageUrl;
-    this.price = price;
-    this.description = description;
-    this.id = id;
-    this.userId = userId;
-  }
-
-
-  async save() {
-    const db = getDb();
-
-    if(this.id) {
-
-      return await db.collection('product').updateOne({
-        _id: new mongodb.ObjectId(this.id),
-      },  { $set: this });
-    }
-
-    return await db.collection('product').insertOne(this);
-  }
+const productSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  imageUrl: {
+    type: String,
+    required: true
+  },
+}); 
 
 
-  static async findAll() {
-    const db = getDb();
-
-    return await db.collection('product').find().toArray();
-  }
-
-
-  static async findById(prodId) {
-    const db = getDb();
-
-    return await db.collection('product').findOne({
-      _id: new mongodb.ObjectId(prodId)
-    });
-  }
-
-
-  static async deleteById(prodId) {
-    const db = getDb();
-
-    return await db.collection('product').deleteOne({
-      _id: new mongodb.ObjectId(prodId)
-    });
-  }
-
-}
-
-
-
-module.exports = Product;
+module.exports = model('Product', productSchema);
